3ab7ced80b76a4bfdc1a16c88e88a0ffe75eaa00
Andres Martin <andres.martin9309@gmail.com>
2026-03-16 19:38:47 -0600

    app: full project v0.1

---
+++ src/routes/+layout.ts
@@ -0,0 +1,2 @@
+export const prerender = true;
+export const ssr = false; // Como usamos Three.js y el DOM, desactivamos el Server Side Rendering
---
+++ svelte.config.js
@@ -0,0 +1,22 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: '404.html',
+      precompress: false,
+      strict: true
+    }),
+    paths: {
+      // Ajusta esto al nombre exacto de tu repositorio en GitHub
+      base: process.env.NODE_ENV === 'production' ? '/vrm-inspector' : '',
+    }
+  }
+};
+
+export default config;
